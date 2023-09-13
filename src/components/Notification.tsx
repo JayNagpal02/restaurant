007eff0c3d1b24145a09a9acaaf1269984884e2c
JayNagpal02 <jaynagpal02@gmail.com>
2023-09-13 18:54:28 +0530

    Navbar completed

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Notification = () => {
-    return <div>Notification</div>;
+    return (
+        <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base">
+            Free delivery for all orders over $50. Order your food now!
+        </div>
+    );
 };
 
 export default Notification;
