1eff65a6e360639a083bbb3301647f1087d53f22
JayNagpal02 <jaynagpal02@gmail.com>
2023-09-14 20:52:55 +0530

    CartPage Design Completed

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,88 @@
+import Image from "next/image";
 import React from "react";
 
 const CartPage = () => {
-    return <div>CartPage</div>;
+    return (
+        <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col text-red-500 lg:flex-row">
+            {/* PRODUCT CONTAINER  */}
+            <div className="h-1/2 p-4 flex flex-col justify-center overflow-scrol lg:h-full lg:w-2/3 2xl:w-1/2 lg:px-20 xl:px-40">
+                {/* SINGLE ITEM */}
+                <div className="flex items-center justify-between mb-4">
+                    <Image
+                        src="/temporary/p1.png"
+                        alt=""
+                        width={90}
+                        height={90}
+                    />
+                    <div className="">
+                        <h1 className="uppercase text-xl font-bold">
+                            sicilian
+                        </h1>
+                        <span>Large</span>
+                    </div>
+                    <h2 className="font-bold">₹799</h2>
+                    <span className="cursor-pointer">X</span>
+                </div>
+                {/* SINGLE ITEM */}
+                <div className="flex items-center justify-between mb-4">
+                    <Image
+                        src="/temporary/p1.png"
+                        alt=""
+                        width={90}
+                        height={90}
+                    />
+                    <div className="">
+                        <h1 className="uppercase text-xl font-bold">
+                            sicilian
+                        </h1>
+                        <span>Large</span>
+                    </div>
+                    <h2 className="font-bold">₹799</h2>
+                    <span className="cursor-pointer">X</span>
+                </div>
+                {/* SINGLE ITEM */}
+                <div className="flex items-center justify-between mb-4">
+                    <Image
+                        src="/temporary/p1.png"
+                        alt=""
+                        width={90}
+                        height={90}
+                    />
+                    <div className="">
+                        <h1 className="uppercase text-xl font-bold">
+                            sicilian
+                        </h1>
+                        <span>Large</span>
+                    </div>
+                    <h2 className="font-bold">₹799</h2>
+                    <span className="cursor-pointer">X</span>
+                </div>
+            </div>
+            {/* PAYMENT CONTAINER  */}
+            <div className="h-1/2 p-4 bg-fuchsia-50 flex flex-col gap-4 justify-center lg:h-full lg:w-1/3 2xl:w-1/2 lg:px-20 xl:px-40 2xl:text-xl 2xl:gap-6">
+                <div className="flex justify-between">
+                    <span className="">Subtotal (3 items)</span>
+                    <span className="">₹817</span>
+                </div>
+                <div className="flex justify-between">
+                    <span className="">Service Cost</span>
+                    <span className="">₹0</span>
+                </div>
+                <div className="flex justify-between">
+                    <span className="">Delivery Cost</span>
+                    <span className="text-green-500">FREE!</span>
+                </div>
+                <hr className="my-2" />
+                <div className="flex justify-between">
+                    <span className="uppercase">Total(INCL. GST)</span>
+                    <span className="font-bold">₹817</span>
+                </div>
+                <button className="uppercase bg-red-500 text-white p-3 rounded-md w-1/2 self-end">
+                    checkout
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default CartPage;
