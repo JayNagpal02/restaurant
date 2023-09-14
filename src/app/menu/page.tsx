5027a11c35120ec55b75102f90801889c0dfdeef
JayNagpal02 <jaynagpal02@gmail.com>
2023-09-14 19:15:18 +0530

    MenuPage & CategoryPage Completed

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const MenuPage = () => {
     return (
-        <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-4rem)] flex flex-col md:flex-row items-center">
+        <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
             {menu.map((category) => (
                 <Link
                     href={`/menu/${category.slug}`}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,10 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-red-500 flex items-center justify-between">
-            <Link href="/" className="font-bold text-xl">MASSIMO</Link>
+        <div className="h-12 md:h-18 p-4 text-red-500 flex items-center justify-between border-t-2 border-red-500">
+            <Link href="/" className="font-bold text-xl">
+                MASSIMO
+            </Link>
             <p>© ALL RIGHTS RESERVED.</p>
         </div>
     );
