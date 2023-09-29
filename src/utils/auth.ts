2569f0aa03df43f28876fa125306c390ff4b6af8
JayNagpal02 <jaynagpal02@gmail.com>
2023-09-29 19:48:07 +0530

    singIn and signOut sucessfully completed

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,16 +3,20 @@ import { NextAuthOptions, getServerSession } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { prisma } from "./connect";
 
+console.log(process.env.GOOGLE_ID);
+console.log(process.env.GOOGLE_SECRET);
+
 export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
-            // clientId: process.env.GOOGLE_ID as string,
-            // clientSecret: process.env.GOOGLE_SECRET as string,
-            clientId: process.env.GOOGLE_ID!,
-            clientSecret: process.env.GOOGLE_SECRET!,
+            clientId: process.env.GOOGLE_ID as string,
+            clientSecret: process.env.GOOGLE_SECRET as string,
+            // clientId: process.env.GOOGLE_ID!,
+            // clientSecret: process.env.GOOGLE_SECRET!,
         }),
     ],
+    // secret: "hello1234567890",
 };
 
 export const getAuthSession = () => getServerSession(authOptions);
