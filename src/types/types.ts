6261520b471b2bb32c0c053a2573ffef0d55b14c
JayNagpal02 <jaynagpal02@gmail.com>
2023-09-30 18:26:07 +0530

    OrderPage completed

--- src/types/types.ts
+++ src/types/types.ts
@@ -20,8 +20,17 @@ export type OrderType = {
     id: string;
     userEmail: string;
     price: number;
-    products: object[];
+    products: CartItemType[];
     status: string;
     createdAt: Date;
     intent_id?: String;
+};
+
+export type CartItemType = {
+    id: string;
+    title: string;
+    img?: string;
+    price: number;
+    optionTitle?: string;
+    quantity: number;
 };
